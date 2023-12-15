5be9b4601d67798ab4a43216a50b829eefea7ee5
Sandeep Kadam <Sandeepkadam3007@gmail.com>
2023-12-15 19:46:18 +0530

    feat: Leaver Server

---
+++ app/api/servers/[serverId]/leave/route.ts
@@ -0,0 +1,49 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+    req: Request, 
+    { params } : { params: { serverId: string } }
+    ){
+
+        try{
+
+            const profile = await currentProfile();
+
+            if(!profile){
+                return new NextResponse("Unauthrized", { status: 401 });
+            }
+
+            if(!params.serverId){
+                return new NextResponse("Server ID not found", { status: 400 });
+            }
+
+            const server = await db.server.update({
+                where:{
+                    id: params.serverId,
+                    profileId: {
+                        not: profile.id // admin cannot leave server they are in 
+                    },
+                    members:{
+                        some:{
+                            profileId: profile.id
+                        }
+                    }
+                },
+                data:{
+                    members:{
+                        deleteMany:{
+                            profileId: profile.id
+                        }
+                    }
+                }
+            })
+
+            return NextResponse.json(server);
+
+        }catch(err){
+            console.log("[SERVER_ID_LEAVE]", err);
+            return new NextResponse("Internal Error", { status: 500 });
+        }
+}
