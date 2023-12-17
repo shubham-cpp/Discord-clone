ff788136292b97e44250443558115525cbf83b5b
Sandeep Kadam <Sandeepkadam3007@gmail.com>
2023-12-17 13:06:37 +0530

    feat: Redirect to channelpage

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,9 +1,50 @@
-const SeverIdPage = () => {
-    return ( 
-        <div>
-            Server ID Page
-        </div> 
-    );
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+interface ServerIdPageProps {
+    params: {
+        serverId: string;
+    }
+}
+
+const SeverIdPage = async ({
+    params
+}: ServerIdPageProps) => {
+
+    const profile = await currentProfile();
+
+    if(!profile) return redirectToSignIn();
+
+    const server = await db.server.findUnique({
+        where:{
+            id: params.serverId,
+            members: {
+                some:{
+                    profileId: profile.id,
+                }
+            }
+        },
+        include:{
+            channels:{
+                where:{
+                    name: "general"
+                },
+                orderBy:{
+                    createdAt: "asc"
+                }
+            }
+        }
+    })
+
+    const initalChannel = server?.channels[0]
+
+    if(initalChannel?.name !== "general"){
+        return null // never woould happen
+    }
+
+    return redirect(`/servers/${params.serverId}/channels/${initalChannel.id}`)
 }
  
 export default SeverIdPage;
