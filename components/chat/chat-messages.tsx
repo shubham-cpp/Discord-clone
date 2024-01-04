4920dee475c164f04300c5506d3dd93f3762fd58
Sandeep Kadam <Sandeepkadam3007@gmail.com>
2024-01-04 19:12:17 +0530

    feat: Chat Socket Hook

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -7,6 +7,7 @@ import { format } from "date-fns"
 import { Loader2, ServerCrash } from "lucide-react";
 import { Fragment } from "react";
 import { ChatItem } from "./chat-item";
+import { useChatSocket } from "@/hooks/use-chat-socket";
 
 const DATE_FORMAT = "d mmm yyyy, HH:mm"
 
@@ -33,8 +34,19 @@ export const ChatMessages = ({
 }: ChatMessageProps) => {
 
     const queryKey = `chat:${chatId}`;
+    const addKey = `chat:${chatId}:messages`;
+    const updateKey= `chat:${chatId}:messages:update`
 
-    const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } = useChatQuery({ queryKey, apiUrl, paramKey, paramValue })
+    
+
+    const { 
+        data,
+        fetchNextPage, 
+        hasNextPage, 
+        isFetchingNextPage, 
+        status } = useChatQuery({ queryKey, apiUrl, paramKey, paramValue })
+
+    useChatSocket({ queryKey, addKey, updateKey })
 
     if(status === "pending"){
         return(
